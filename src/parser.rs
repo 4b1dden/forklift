4f867aa5be1d1f951f01ba5bc151b3c0ac88f593
Kiko Čabala <kristiancabala@gmail.com>
2022-06-13 16:39:31 +0200

    add Parser trait monad-like functionality && prototype: Parse binary expressions into AST

--- src/parser.rs
+++ src/parser.rs
@@ -23,6 +23,99 @@ fn parse_number(input: &str) -> ParseResult<&str> {
 
 trait Parser<'a, Output> {
     fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+
+    fn map<F, MOut>(self, map_fn: F) -> BoxedParser<'a, MOut>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+        MOut: 'a,
+        F: Fn(Output) -> MOut + 'a,
+    {
+        BoxedParser::new(map(self, map_fn))
+    }
+
+    fn predicate<F>(self, pred_fn: F) -> BoxedParser<'a, Output>
+    where
+        Self: Sized + 'a,
+        Output: 'a + Debug,
+        F: Fn(&Output) -> bool + 'a,
+    {
+        BoxedParser::new(predicate(self, pred_fn))
+    }
+
+    fn and_then<F, NextParser, NewOutput>(self, f: F) -> BoxedParser<'a, NewOutput>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+        NewOutput: 'a,
+        NextParser: Parser<'a, NewOutput> + 'a,
+        F: Fn(Output) -> NextParser + 'a,
+    {
+        BoxedParser::new(and_then(self, f))
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum BinaryOperator {
+    Plus,
+    Minus,
+    Div,
+    Mul,
+}
+
+impl From<&str> for BinaryOperator {
+    fn from(incoming: &str) -> Self {
+        match incoming {
+            "+" => BinaryOperator::Plus,
+            "-" => BinaryOperator::Minus,
+            "/" => BinaryOperator::Div,
+            "*" => BinaryOperator::Mul,
+            _ => panic!("not implemented"),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Expr {
+    Literal(LiteralExpr),
+    Binary(BinaryExpr),
+}
+
+#[derive(Clone, Debug)]
+pub struct LiteralExpr(i32);
+
+#[derive(Clone, Debug)]
+pub struct BinaryExpr {
+    pub lhs: Box<Expr>,
+    pub op: BinaryOperator,
+    pub rhs: Box<Expr>,
+}
+
+fn and_then<'a, P, F, A, B, NextP>(parser: P, f: F) -> impl Parser<'a, B>
+where
+    P: Parser<'a, A>,
+    NextP: Parser<'a, B>,
+    F: Fn(A) -> NextP,
+{
+    move |input| match parser.parse(input) {
+        Ok((next_input, result)) => f(result).parse(next_input),
+        Err(err) => Err(err),
+    }
+}
+
+pub struct BoxedParser<'a, Output> {
+    parser: Box<dyn Parser<'a, Output> + 'a>,
+}
+
+impl<'a, Output> BoxedParser<'a, Output> {
+    fn new<P>(parser: P) -> Self
+    where
+        P: Parser<'a, Output> + 'a,
+    {
+        BoxedParser {
+            parser: Box::new(parser),
+        }
+    }
 }
 
 impl<'a, F, Output> Parser<'a, Output> for F
@@ -31,6 +124,12 @@ where
 {
     fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
         self(input)
+    }
+}
+
+impl<'a, Output> Parser<'a, Output> for BoxedParser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+        self.parser.parse(input)
     }
 }
 
@@ -126,7 +225,7 @@ where
     }
 }
 
-fn bin_op<'a>() -> impl Parser<'a, &'a str> {
+fn bin_operand<'a>() -> impl Parser<'a, &'a str> {
     let operands = vec!["+", "-", "/", "*"];
     any_of_monomorphic(
         operands
@@ -215,12 +314,11 @@ fn optional_whitespace<'a>() -> impl Parser<'a, Vec<char>> {
 
 fn trim_whitespace_around<'a, P, R>(parser: P) -> impl Parser<'a, R>
 where
-    P: Parser<'a, R>,
+    P: Parser<'a, R> + 'a,
+    R: 'a,
 {
-    map(
-        triplet(optional_whitespace(), parser, optional_whitespace()),
-        |(_, inside_result, _)| inside_result,
-    )
+    triplet(optional_whitespace(), parser, optional_whitespace())
+        .map(|(_, inside_result, _)| inside_result)
 }
 
 fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
@@ -231,15 +329,123 @@ where
     map(pair(parser1, parser2), |(_, right_result)| right_result)
 }
 
+// Todo: add parentheses
+fn parse_binary_expression<'a>() -> impl Parser<'a, (&'a str, Vec<(BinaryOperator, &'a str)>)> {
+    let lhs_number = trim_whitespace_around(parse_number);
+    let bin_op = trim_whitespace_around(bin_operand()).map(|op| BinaryOperator::from(op));
+    let rhs_number = trim_whitespace_around(parse_number);
+
+    let rhs_parser = pair(bin_op, rhs_number);
+
+    return pair(lhs_number, one_or_more(rhs_parser));
+}
+
+// TODO: get rid of this
+#[derive(Debug)]
+pub enum BinExpInfix {
+    Literal(LiteralExpr),
+    Op(BinaryOperator),
+}
+
+#[derive(Debug)]
+pub struct ASTNode {
+    pub left: Option<Box<ASTNode>>,
+    pub right: Option<Box<ASTNode>>,
+    pub val: BinExpInfix,
+}
+
+fn get_single_operator_precedence(op: &BinaryOperator) -> i32 {
+    match op {
+        BinaryOperator::Plus | BinaryOperator::Minus => 1,
+        BinaryOperator::Mul | BinaryOperator::Div => 2,
+    }
+}
+
+/// 1 is op2, -1 is op1
+fn compare_operator_precedence(op1: &BinaryOperator, op2: &BinaryOperator) -> i32 {
+    if get_single_operator_precedence(op2) > get_single_operator_precedence(op1) {
+        1
+    } else {
+        -1
+    }
+}
+
+/// Shunting yard algorithm
+/// https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+/// thank mr dijsktra
+/// https://www.klittlepage.com/2013/12/22/twelve-days-2013-shunting-yard-algorithm/
+fn infix_to_ast(infix: Vec<BinExpInfix>) -> ASTNode {
+    let mut operator_stack = vec![];
+    let mut operand_stack: Vec<ASTNode> = vec![];
+
+    for infix_item in infix.iter() {
+        match infix_item {
+            BinExpInfix::Literal(literal) => {
+                operand_stack.push(ASTNode {
+                    left: None,
+                    right: None,
+                    val: BinExpInfix::Literal(literal.clone()),
+                });
+            }
+            BinExpInfix::Op(operator1) => {
+                while let Some(top) = operator_stack.last() {
+                    if compare_operator_precedence(operator1, top) == 1 {
+                        if let Some(popped) = operator_stack.pop() {
+                            add_node(&mut operand_stack, popped);
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                operator_stack.push(operator1.clone());
+            }
+        };
+    }
+
+    while !operator_stack.is_empty() {
+        add_node(&mut operand_stack, operator_stack.pop().unwrap());
+    }
+
+    operand_stack.pop().unwrap()
+}
+
+fn add_node(stack: &mut Vec<ASTNode>, op: BinaryOperator) {
+    let right = stack.pop().unwrap();
+    let left = stack.pop().unwrap();
+    stack.push(ASTNode {
+        left: Some(Box::new(left)),
+        right: Some(Box::new(right)),
+        val: BinExpInfix::Op(op),
+    });
+}
+
+// Todo: get rid of this completely
+pub fn parse_binary_to_infix<'a>(
+    result: (&'a str, (&'a str, Vec<(BinaryOperator, &'a str)>)),
+) -> Vec<BinExpInfix> {
+    let mut infix = vec![];
+
+    infix.push(BinExpInfix::Literal(LiteralExpr(
+        result.1 .0.parse().unwrap(),
+    )));
+
+    for item in result.1 .1.iter() {
+        infix.push(BinExpInfix::Op(item.0.clone()));
+        infix.push(BinExpInfix::Literal(LiteralExpr(item.1.parse().unwrap())));
+    }
+
+    infix
+}
+
 #[cfg(test)]
 mod tests {
-
-    use crate::parser::bin_op;
-
     use super::{
-        any_of_monomorphic, at_least_one_whitespace, left, map, one_or_more, optional_whitespace,
-        pair, parse_literal, parse_number, predicate, right, trim_whitespace_around, triplet,
-        zero_or_more, GrammarItem, Parser,
+        any_of_monomorphic, at_least_one_whitespace, bin_operand, infix_to_ast, left, map,
+        one_or_more, optional_whitespace, pair, parse_binary_expression, parse_binary_to_infix,
+        parse_literal, parse_number, predicate, right, trim_whitespace_around, triplet,
+        zero_or_more, BinExpInfix, BinaryExpr, BinaryOperator, Expr, GrammarItem, LiteralExpr,
+        Parser,
     };
 
     #[test]
@@ -362,23 +568,39 @@ mod tests {
         assert!(parser.parse(src2).is_err());
     }
 
+    const BINARY_EXPR_DEFAULT: &str = "   3 * 100    + 4 * 2";
     #[test]
-    fn test_plus_seq() {
-        // todo:compose properly
-        fn plus_seq<'a>() -> impl Parser<'a, (&'a str, &'a str, &'a str)> {
-            let lhs_number = trim_whitespace_around(parse_number);
-            let bin_op = trim_whitespace_around(bin_op());
-            let rhs_number = trim_whitespace_around(parse_number);
+    fn test_parse_binary_expression() {
+        let parser = parse_binary_expression();
 
-            triplet(lhs_number, bin_op, rhs_number)
-        }
+        let result = parser.parse(BINARY_EXPR_DEFAULT).unwrap();
 
-        let parser = plus_seq();
+        assert_eq!(
+            (
+                "",
+                (
+                    "3",
+                    vec![
+                        (BinaryOperator::Mul, "100"),
+                        (BinaryOperator::Plus, "4"),
+                        (BinaryOperator::Mul, "2")
+                    ]
+                )
+            ),
+            result
+        );
+    }
 
-        println!("{:#?}", parser.parse("   1 + 2  "));
-        println!("{:#?}", parser.parse("   32 - 45  "));
-        println!("{:#?}", parser.parse("   4 / 2  "));
-        println!("{:#?}", parser.parse("   15 * 3  "));
+    #[test]
+    fn test_binary_expression_to_ast() {
+        let parser = parse_binary_expression();
+        let parsed = parser.parse(BINARY_EXPR_DEFAULT).unwrap();
+        let infix = parse_binary_to_infix(parsed);
+
+        let ast = infix_to_ast(infix);
+
+        println!("{:#?}", ast);
+        // TODO: write test case once ASTNode type semantics are stabilised
     }
 
     #[test]
